df633d384b310fddf2b2e5ba910236bb593ee828
balu-pal <balu.sakari@gmail.com>
2018-04-17 09:33:03 -0600

    Pal Tracker Distributed - Check in

---
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -0,0 +1,37 @@
+package io.pivotal.pal.tracker.backlog;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestOperations;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.TimeZone;
+
+
+@SpringBootApplication
+@ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableWebSecurity
+@EnableResourceServer
+public class App {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ProjectClient projectClient(
+        RestOperations restOperations,
+        @Value("${registration.server.endpoint}") String registrationEndpoint
+    ) {
+        return new ProjectClient(restOperations, registrationEndpoint);
+    }
+}
